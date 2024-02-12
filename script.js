b20fb1d17ff1dd37c835d6928ecc61abc7863da7
AlinkaSuper <alinka31frolova@gmail.com>
2024-02-12 09:45:33 +0200

    Valentine

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function randomPosition () {
     let containerRect = container.getBoundingClientRect();
     let buttonRect = button.getBoundingClientRect();
 
-    let maxX = containerRect.width - buttonRect.width;
-    let maxY = containerRect.height - buttonRect.height;
+    let maxX = 100;
+    let maxY = 100;
 
     let randomX = Math.max(0, Math.min(maxX, Math.floor(Math.random() * maxX)));
     let randonY = Math.max(0, Math.min(maxY, Math.floor(Math.random() * maxY)));
 
-    button.style.left = randomX + 'px';
-    button.style.top = randonY + 'px';
+    button.style.left = randomX + '%';
+    button.style.top = randonY + '%';
 }
